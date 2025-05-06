17d45c2e56c4d01b835b7d8be7802212e69269ea
Lucas pereira <lucas_trucoo@hotmail.com>
2025-05-05 23:43:12 -0300

    feat: adicionado entidade de venda com regras de atualização e adição de items e também configurado entidade no entityFramework

---
+++ template/backend/src/Ambev.DeveloperEvaluation.Domain/Entities/Sale.cs
@@ -0,0 +1,116 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Enums;
+
+namespace Ambev.DeveloperEvaluation.Domain.Entities;
+
+public class Sale
+{
+    protected Sale()
+    {
+        CreateAt = DateTime.UtcNow;
+        Status = SaleStatus.Pending;
+        Items = [];
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; set; }
+
+    public decimal TotalValue { get; private set; }
+
+    public DateTime CreateAt { get; set; }
+
+    public Guid CustomerId { get; set; }
+
+    public string CustomerName { get; set; }
+
+    public Guid BranchId { get; set; }
+    public string BranchName { get; set; }
+
+    public SaleStatus Status { get; set; }
+
+    public ICollection<SaleItem> Items { get; private set; }
+
+
+    public static Sale CreateSale(Guid customerId, string customerName, Guid branchId, string branchName)
+    {
+        return new Sale()
+        {
+            CustomerId = customerId,
+            CustomerName = customerName,
+            BranchId = branchId,
+            BranchName = branchName,
+        };
+    }
+
+    public void AddItem(SaleItem saleItem)
+    {
+        Items.Add(saleItem);
+
+        TotalValue += saleItem.GetTotalValueWithDiscount();
+    }
+
+    public void UpdateItem(Guid prodcutId, decimal unitPrice, short quantity, decimal discount)
+    {
+        var item = Items.First(x => x.ProductId.Equals(prodcutId));
+
+        item.UpdateItem(unitPrice, quantity, discount);
+
+        RecalculateValueTotalItems();
+    }
+
+    public void CancelItem(Guid productId)
+    {
+        Items.First(x => x.ProductId == productId).Cancel();
+
+        RecalculateValueTotalItems();
+    }
+
+    private void RecalculateValueTotalItems() => TotalValue = Items
+                                                             .Where(x => !x.Cancelled)
+                                                             .Sum(x => x.GetTotalValueWithDiscount());
+
+}
+
+public class SaleItem
+{
+    public SaleItem()
+    {
+
+    }
+    public SaleItem(Guid saleId, Guid productId, decimal unitPrice, short quantity, decimal discount)
+    {
+        SaleId = saleId;
+        ProductId = productId;
+        UnitPrice = unitPrice;
+        Quantity = quantity;
+        Discount = discount;
+    }
+
+    public Guid SaleId { get; set; }
+
+    public Guid ProductId { get; set; }
+
+    public decimal UnitPrice { get; set; }
+
+    public short Quantity { get; set; }
+
+    public decimal Discount { get; set; }
+
+    public bool Cancelled { get; set; }
+
+    public void UpdateItem(decimal price, short quantity, decimal discount)
+    {
+        Quantity = quantity;
+        UnitPrice = price;
+        Discount = discount;
+    }
+
+    public void Cancel() => Cancelled = true;
+
+    public decimal GetTotalValueWithDiscount() => (UnitPrice * Quantity) - Discount;
+
+    public decimal GetTotalValue() => (UnitPrice * Quantity);
+
+
+    //Entity Framework
+    public Sale Sale { get; set; }
+}
---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping
+{
+    public class SaleConfiguration : IEntityTypeConfiguration<Sale>
+    {
+        public void Configure(EntityTypeBuilder<Sale> builder)
+        {
+            builder.ToTable("Sales");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.Status)
+                .HasConversion<string>()
+                .HasMaxLength(20);
+
+            builder.Property(s => s.BranchId)
+                .IsRequired();
+
+            builder.Property(s => s.CustomerId)
+                .IsRequired();
+
+            builder.Property(s => s.BranchName)
+                .HasMaxLength(50);
+
+            builder.Property(s => s.TotalValue)
+                .HasPrecision(18, 2);
+
+            builder.Property(s => s.CustomerName)
+                .HasMaxLength(100);
+
+            builder.HasMany(s => s.Items)
+                .WithOne(s => s.Sale)
+                .HasForeignKey(s =>  s.SaleId)
+                .OnDelete(DeleteBehavior.NoAction);
+
+        }
+    }
+
+    public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+    {
+        public void Configure(EntityTypeBuilder<SaleItem> builder)
+        {
+            builder.ToTable("SaleItems");
+
+            builder.HasKey(s => new { s.SaleId, s.ProductId });
+
+            builder.Property(s => s.UnitPrice)
+                .HasPrecision(10, 2);
+
+            builder.HasOne(s => s.Sale)
+                .WithMany(s => s.Items)
+                .HasForeignKey(si => si.SaleId);
+        }
+    }
+}
