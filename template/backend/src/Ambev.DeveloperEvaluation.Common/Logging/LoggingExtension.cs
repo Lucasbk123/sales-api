35973a4ec916a48ab00cf5110918395aa4da1ff0
Lucas pereira <lucas_trucoo@hotmail.com>
2025-05-12 00:37:55 -0300

    fix: ajustes no log de error

--- template/backend/src/Ambev.DeveloperEvaluation.Common/Logging/LoggingExtension.cs
+++ template/backend/src/Ambev.DeveloperEvaluation.Common/Logging/LoggingExtension.cs
@@ -31,7 +31,8 @@ public static class LoggingExtension
     static readonly Func<LogEvent, bool> _filterPredicate = exclusionPredicate =>
     {
 
-        if (exclusionPredicate.Level != LogEventLevel.Information) return true;
+        if (exclusionPredicate.Level != LogEventLevel.Information && exclusionPredicate.Level != LogEventLevel.Error) 
+            return true;
 
         exclusionPredicate.Properties.TryGetValue("StatusCode", out var statusCode);
         exclusionPredicate.Properties.TryGetValue("Path", out var path);
